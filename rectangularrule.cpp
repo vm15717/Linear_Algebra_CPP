ecc6b2345be201f6a3c905ba15048794703ee6f1
vm15717 <vm15717@my.bristol.ac.uk>
2025-01-19 13:04:22 +0530

    the rectangular rule code calculates the right rectangular rule for integration

--- rectangularrule.cpp
+++ rectangularrule.cpp
@@ -7,19 +7,18 @@ double exp2(double);
 int main()
 {
     std::cout << "Rectangular rule: " << std::endl;
-    int num = 10000;
+    int num = 1000001;
     std::vector <double> result(num, 0.0);
     int a = 0;
     int b = 1;
     double dx = (b - a) /(double) num;
-    result[0] = rectangular(exp2, 0) * dx;
-    for (int i = 1; i < num; i++)
+    for (int i = 1; i <= num; i++)
     {
         result[i] = result[i-1] + rectangular(exp2, a + dx * i) * dx;
     }
-    for (int j = 0; j <= num; j += num/10)
+    for (int j = 0; j <= num; j += num/100)
     {
-        std::cout << j << "th step : " << result[j-1] << std::endl;
+        std::cout << j << "th step : " << result[j] << std::endl;
     }
     return 0;
 }
