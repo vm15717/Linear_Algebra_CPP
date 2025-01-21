1c3759d516deb9c1711e606fbfcc63b2d1000106
vm15717 <vm15717@my.bristol.ac.uk>
2025-01-21 19:02:40 +0530

    added sqrt function to central difference

--- centraldifference.cpp
+++ centraldifference.cpp
@@ -4,7 +4,7 @@
 double exp2(double );
 double rightdiff(double (*f)(double), double, double);
 double prodfunc(double );
-
+double sqrt2(double );
 int main()
 {
     double a = 1.0;
@@ -12,8 +12,11 @@ int main()
     double adiff = rightdiff(exp2, a, h);
     double b = 0.1;
     double bdiff = rightdiff(prodfunc, b, h);
+    double c = 0.5;
+    double cdiff = rightdiff(sqrt2, c, h);
     std::cout << std::setprecision(17) << adiff << std::endl;
     std::cout << std::setprecision(17) << bdiff << std::endl;
+    std::cout << std::setprecision(17) << cdiff << std::endl;
     return 0;
 }
 
@@ -35,4 +38,9 @@ double prodfunc(double x)
         res = res * (x/i - 1);
     }
     return res;
+}
+
+double sqrt2(double x)
+{
+    return std::sqrt(x);
 }
