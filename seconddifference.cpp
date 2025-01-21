4ee028a91b1afa58d08039c28d1d87c3d28e7d0d
vm15717 <vm15717@my.bristol.ac.uk>
2025-01-21 19:06:27 +0530

    added sqrt function to second difference

--- seconddifference.cpp
+++ seconddifference.cpp
@@ -3,13 +3,17 @@
 
 double seconddiff(double (*f)(double), double, double);
 double exp2(double);
+double sqrt2(double);
 
 int main()
 {
     double a = 1.0;
     double h = 1e-5;
+    double b = 2.0;
     double expdiff = seconddiff(exp2, a, h)/std::pow(h, 2);
     std::cout << std::setprecision(17) << expdiff << std::endl;
+    double sqrtdiff = seconddiff(sqrt2, b, h)/std::pow(h, 2);
+    std::cout << std::setprecision(17) << sqrtdiff << std::endl;
     return 0;
 }
 
@@ -21,4 +25,9 @@ double exp2(double x)
 double seconddiff(double (*f)(double), double x, double h)
 {
     return f(x+h)-2*f(x)+f(x-h);
+}
+
+double sqrt2(double x)
+{
+    return std::sqrt(x);
 }
