2b5068466f92e81b6eb1675bfd76b3703a1df136
vm15717 <vm15717@my.bristol.ac.uk>
2025-01-23 19:32:13 +0000

    not much change second difference

--- seconddifference.cpp
+++ seconddifference.cpp
@@ -7,9 +7,9 @@ double sqrt2(double);
 double sin2(double);
 int main()
 {
-    double a = 1.0;
-    double h = 1e-5;
-    double b = 2.0;
+    double a = 1.5;
+    double h = 1e-7;
+    double b = 31;
     double expdiff = seconddiff(exp2, a, h)/std::pow(h, 2);
     std::cout << std::setprecision(17) << expdiff << std::endl;
     double sqrtdiff = seconddiff(sqrt2, b, h)/std::pow(h, 2);
