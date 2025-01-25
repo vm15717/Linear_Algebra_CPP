83020b75d26517807e0def02284ab5199e0cfe3a
vm15717 <vm15717@my.bristol.ac.uk>
2025-01-25 15:32:16 +0000

    testing division in dual numbers

--- testDual.cpp
+++ testDual.cpp
@@ -4,6 +4,9 @@
 int main()
 {
     Dual d(3, 1);
+    Dual d1(0, 2);
+    Dual d2(0, 3);
     std::cout << d.apply(std::sin, std::cos) << std::endl;
+    std::cout << d1/d2 << std::endl;
     return 0;
 }
